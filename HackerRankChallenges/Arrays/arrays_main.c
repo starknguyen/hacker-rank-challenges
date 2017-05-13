d723c29ad84101c2348ffb2175efd43ed7ef5bb3
starknguyen <ngdinhnghi@gmail.com>
2017-05-13 11:33:17 +0300

    Initial commit

---
+++ HackerRankChallenges/Arrays/arrays_main.c
@@ -0,0 +1,8 @@
+#include "arrays_main.h"
+
+int main(char argc, char* argv[])
+{
+    one_dimensional_array();
+    
+    return 0;
+}
---
+++ HackerRankChallenges/Arrays/arrays_main.h
@@ -0,0 +1,2 @@
+#pragma once
+void one_dimensional_array(void);

102416b1153dd8e5e9adecf75d9c8b9adc8d15c5
NghiNguyen <ngdinhnghi@gmail.com>
2019-04-28 16:57:50 +0300

    Solve binary numbers problem in 30 days of code

---
+++ HackerRankChallenges/30DaysCode/HRC.30DaysCode/BinaryNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRC.Code30Days
+{
+    /// <summary>
+    /// Given a base-10 integer, n, convert it to binary (base-1). 
+    /// Then find and print the base-10 integer denoting the maximum number of consecutive 1's in n's binary representation.
+    /// </summary>
+    public class BinaryNumbers
+    {
+        public int GetMaxNumberOfOne(int n)
+        {
+            // TEST CASE: 439
+            string nBinary = Convert.ToString(n, 2);
+            int countOne = 0;
+            int maxTemp = 0;
+            for (int i = 0; i < nBinary.Length; i++)
+            {
+                if (nBinary[i] == '0')
+                {
+                    if (countOne > maxTemp)
+                    {
+                        maxTemp = countOne;
+                    }
+
+                    countOne = 0;
+                }
+                else
+                    countOne++;
+            }
+
+            return (countOne > maxTemp) ? countOne : maxTemp;
+        }
+    }
+}
---
+++ HackerRankChallenges/30DaysCode/HRC.30DaysCode/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRC.Code30Days
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                testBinaryNumbers();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR - " + ex);
+            }
+
+            Console.ReadKey();
+        }
+
+
+        private static void testBinaryNumbers()
+        {
+            var input = Convert.ToInt32(Console.ReadLine());
+
+            var bn = new BinaryNumbers();
+            var result = bn.GetMaxNumberOfOne(input);
+            Console.WriteLine(result);
+        }
+    }
+}

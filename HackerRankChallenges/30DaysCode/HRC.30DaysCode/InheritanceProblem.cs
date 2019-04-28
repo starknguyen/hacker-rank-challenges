f692f994cc7d4f1bebe6413fbb8e1fb7655488e4
NghiNguyen <ngdinhnghi@gmail.com>
2019-04-28 18:05:35 +0300

    Solve Inheritance problem

---
+++ HackerRankChallenges/30DaysCode/HRC.30DaysCode/InheritanceProblem.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRC.Code30Days
+{
+    public class InheritanceProblem
+    {
+        public class Person
+        {
+            protected string firstName;
+            protected string lastName;
+            protected int id;
+
+            public Person() { }
+            public Person(string firstName, string lastName, int identification)
+            {
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.id = identification;
+            }
+            public void printPerson()
+            {
+                Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id);
+            }
+        }
+
+
+        public class Student : Person{
+            private int[] testScores;           
+
+            /*	
+            *   Class Constructor
+            *   
+            *   Parameters: 
+            *   firstName - A string denoting the Person's first name.
+            *   lastName - A string denoting the Person's last name.
+            *   id - An integer denoting the Person's ID number.
+            *   scores - An array of integers denoting the Person's test scores.
+            */
+            // Write your constructor here
+            public Student(string firstName, string lastName, int id, int[] scores) : base(firstName, lastName, id)
+            {
+                testScores = scores;
+            }
+
+            /*	
+            *   Method Name: Calculate
+            *   Return: A character denoting the grade.
+            */
+            // Write your method here
+            public char Calculate()
+            {
+                int sum = 0;
+                for (int i = 0; i < testScores.Length; i++)
+                {
+                    sum += testScores[i];
+                }
+
+                int avgSum = sum / testScores.Length;
+                if (avgSum < 40)
+                    return 'T';
+                if (avgSum >= 40 && avgSum < 55)
+                    return 'D';
+                if (avgSum >= 55 && avgSum < 70)
+                    return 'P';
+                if (avgSum >= 70 && avgSum < 80)
+                    return 'A';
+                if (avgSum >= 80 && avgSum < 90)
+                    return 'E';
+                if (avgSum >= 90 && avgSum <= 100)
+                    return 'O';
+
+                throw new Exception("Unsupported avgSum: " + avgSum);
+            }
+        }
+
+
+    }
+}
--- HackerRankChallenges/30DaysCode/HRC.30DaysCode/Program.cs
+++ HackerRankChallenges/30DaysCode/HRC.30DaysCode/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static HRC.Code30Days.InheritanceProblem;
 
 namespace HRC.Code30Days
 {
@@ -13,8 +14,8 @@ namespace HRC.Code30Days
             try
             {
                 //testBinaryNumbers();
-                test2DArrayHourGlass();
-
+                //test2DArrayHourGlass();
+                testInheritance();
             }
             catch (Exception ex)
             {
@@ -24,6 +25,24 @@ namespace HRC.Code30Days
             Console.ReadKey();
         }
 
+        private static void testInheritance()
+        {
+            string[] inputs = Console.ReadLine().Split();
+            string firstName = inputs[0];
+            string lastName = inputs[1];
+            int id = Convert.ToInt32(inputs[2]);
+            int numScores = Convert.ToInt32(Console.ReadLine());
+            inputs = Console.ReadLine().Split();
+            int[] scores = new int[numScores];
+            for (int i = 0; i < numScores; i++)
+            {
+                scores[i] = Convert.ToInt32(inputs[i]);
+            }
+
+            Student s = new Student(firstName, lastName, id, scores);
+            s.printPerson();
+            Console.WriteLine("Grade: " + s.Calculate() + "\n");
+        }
 
         private static void test2DArrayHourGlass()
         {
@@ -49,6 +68,7 @@ namespace HRC.Code30Days
             var max = hourGlass.GetMaxHourGlassSum(arr);
             Console.WriteLine(max);
         }
+
 
         private static void testBinaryNumbers()
         {

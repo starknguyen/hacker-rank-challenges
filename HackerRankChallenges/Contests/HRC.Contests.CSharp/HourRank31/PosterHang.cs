7d2b9670da97e834f267970b9288377d9b0d2b2e
starknguyen <ngdinhnghi@gmail.com>
2019-05-01 21:44:00 +0300

    Add Contest project and solve PosterHand problem from HourRank31

---
+++ HackerRankChallenges/Contests/HRC.Contests.CSharp/HourRank31/PosterHang.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRC.Contests.CSharp.HourRank31
+{
+    public class PosterHang
+    {
+        public int solve(int h, List<int> wallPoints, List<int> lengths)
+        {
+            List<KeyValuePair<int, int>> dict = new List<KeyValuePair<int, int>>();
+            for (int i = 0; i < wallPoints.Count(); i++)
+                dict.Add(new KeyValuePair<int, int>(wallPoints[i], lengths[i]));
+
+            int heightRequired = 0;
+
+            foreach (var item in dict)
+            {
+                int heightToReach = item.Key - (item.Value / 4);
+                if (heightToReach < 5)
+                    continue;
+                int temp = Convert.ToInt32(Math.Ceiling((double)heightToReach - h));
+                if (temp > heightRequired)
+                    heightRequired = temp;
+            }
+
+            return heightRequired;
+        }
+    }
+}
---
+++ HackerRankChallenges/Contests/HRC.Contests.CSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using HRC.Contests.CSharp.HourRank31;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRC.Contests.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testPosterHangProblem();
+        }
+
+
+        private static void testPosterHangProblem()
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+            int h = Convert.ToInt32(firstMultipleInput[1]);
+            List<int> wallPoints = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(wallPointsTemp => Convert.ToInt32(wallPointsTemp)).ToList();
+            List<int> lengths = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(lengthsTemp => Convert.ToInt32(lengthsTemp)).ToList();
+
+            var postetHand = new PosterHang();
+            int answer = postetHand.solve(h, wallPoints, lengths);
+        }
+
+    }
+}

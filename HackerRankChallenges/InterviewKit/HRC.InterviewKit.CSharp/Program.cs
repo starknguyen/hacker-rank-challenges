6e370db6dd46c2d172864dd9949025e2e1f5a142
NghiNguyen <ngdinhnghi@gmail.com>
2019-05-04 12:40:19 +0300

    Solve sock merchant problem

---
+++ HackerRankChallenges/InterviewKit/HRC.InterviewKit.CSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRC.InterviewKit.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                testSockMerchant();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex);
+            }
+        }
+
+
+        private static void testSockMerchant()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+
+            var sm = new SockMerchant();
+            int result = sm.sockMerchant(n, ar);
+        }
+    }
+}
---
+++ HackerRankChallenges/InterviewKit/HRC.InterviewKit.CSharp/SockMerchant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRC.InterviewKit.CSharp
+{
+    public class SockMerchant
+    {
+        // Complete the sockMerchant function below.
+        public int sockMerchant(int n, int[] ar)
+        {
+            int pairCount = 0;
+            List<int> arList = ar.ToList();
+            List<int> disctinctList = ar.Distinct().ToList();
+            
+            for (int i = 0; i < disctinctList.Count(); i++)
+            {
+                int count = arList.Where(s => s == disctinctList[i]).Count();
+                pairCount += (count / 2);
+            }
+
+            return pairCount;
+        }        
+    }
+}
